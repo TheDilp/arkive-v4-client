ed8311bebb0a46eb63c31e6892bdebd054317d87
= <=>
2023-07-23 12:02:55 +0200

    feat: custom button with atom

--- src/components/Overlay/Dialog.tsx
+++ src/components/Overlay/Dialog.tsx
@@ -22,7 +22,7 @@ const DialogClasses = tv({
     container: "pointer-events-none flex h-full w-full animate-in fade-in",
     base: "flex flex-col pointer-events-auto rounded bg-zinc-700 text-white mx-4 border-zinc-600 border-b shadow-lg p-4",
     titleContainer: "flex items-center",
-    title: "w-full flex justify-center truncate font-merriweather text-3xl select-none",
+    title: "w-full flex justify-center font-merriweather text-3xl select-none text-center",
     imageUploadContainer: "sticky top-0 bg-zinc-700",
     imagesList: "grid grid-cols-2 gap-2 overflow-y-auto",
   },
@@ -104,6 +104,26 @@ export function Dialog() {
               ) : null}
               {dialog.type === "archive_entity" || dialog.type === "delete_entity" ? (
                 <DeleteEntityDialog data={dialog.data} type={dialog.type} />
+              ) : null}
+              {dialog?.cancel || dialog?.confirm ? (
+                <div className="mt-auto flex items-center justify-center gap-x-2">
+                  {dialog?.cancel ? (
+                    <Button
+                      icon={dialog?.cancel?.icon || IconEnum.close}
+                      label={dialog?.cancel?.label || "Cancel"}
+                      onClick={dialog?.cancel?.action}
+                      variant={dialog?.cancel?.variant || "secondary"}
+                    />
+                  ) : null}
+                  {dialog?.confirm ? (
+                    <Button
+                      icon={dialog?.confirm?.icon || IconEnum.close}
+                      label={dialog?.confirm?.label || "Confirm"}
+                      onClick={dialog?.confirm?.action}
+                      variant={dialog?.confirm?.variant || "error"}
+                    />
+                  ) : null}
+                </div>
               ) : null}
             </div>
           </div>
