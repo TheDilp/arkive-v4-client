e31f563533895f4f7b51d351615f867dd5a7cce0
= <=>
2023-07-30 13:49:59 +0200

    chore: delete unused component

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,7 +1,6 @@
 export * from "./Breadcrumbs";
 export * from "./Card";
 export * from "./Collapsible";
-export * from "./FolderView";
 export * from "./Layout";
 export * from "./Navbar";
 export * from "./Sidebar";
--- src/pages/Entities/index.ts
+++ src/pages/Entities/index.ts
@@ -1,3 +1,2 @@
 export * from "./CharactersView";
 export * from "./EntitiesView";
-export * from "./GraphsView";
