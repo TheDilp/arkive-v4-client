0d52075e6e1c1da6273da8f0977d4ff994b5b3e8
= <=>
2023-08-03 08:34:09 +0200

    feat: document validation

---
+++ src/validation/document.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const InsertDocumentSchema = z.object({
+  data: z.object({
+    project_id: z.string(),
+    title: z.string().optional(),
+    content: z.string().nullable().optional(),
+    icon: z.string().nullable().optional(),
+    is_folder: z.boolean().nullable().optional(),
+    is_public: z.boolean().nullable().optional(),
+    is_template: z.boolean().nullable().optional(),
+    parent_id: z.string().nullable().optional(),
+    image_id: z.string().nullable().optional(),
+  }),
+  relations: z
+    .object({
+      tags: z.object({ id: z.string() }).array().optional(),
+      alter_names: z.object({ title: z.string() }).array().optional(),
+    })
+    .optional(),
+});
+export const UpdateDocumentSchema = z.object({
+  title: z.string().optional(),
+  content: z.string().nullable().optional(),
+  icon: z.string().nullable().optional(),
+  is_folder: z.boolean().nullable().optional(),
+  is_public: z.boolean().nullable().optional(),
+  is_template: z.boolean().nullable().optional(),
+  parent_id: z.string().nullable().optional(),
+  image_id: z.string().nullable().optional(),
+});
