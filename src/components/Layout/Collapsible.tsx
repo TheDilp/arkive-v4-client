8e739133739ff3ef1bc5da9782695f37e68dea6d
= <=>
2023-08-15 10:25:12 +0200

    feat: add actions for collapsible

--- src/components/Layout/Collapsible.tsx
+++ src/components/Layout/Collapsible.tsx
@@ -2,9 +2,9 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { CollapsibleType } from "../../types";
 import { IconEnum } from "../../utils";
-import { Icon } from "..";
+import { Button, Icon } from "..";
 
-export function Collapsible({ label, children }: CollapsibleType) {
+export function Collapsible({ label, children, actions }: CollapsibleType) {
   return (
     <details className="select-none">
       <summary className="flex items-center gap-x-2 border-b border-zinc-700 pb-1 ">
@@ -12,6 +12,20 @@ export function Collapsible({ label, children }: CollapsibleType) {
         <span className="ml-auto">
           <Icon fontSize={28} icon={IconEnum.chevron_up} />
         </span>
+        {actions?.length
+          ? actions.map((act) => (
+              <div key={act.label || act.icon}>
+                <Button
+                  hasNoBackground
+                  icon={act?.icon}
+                  label={act?.label}
+                  onClick={act.onClick}
+                  tooltip={act?.tooltip}
+                  variant={act?.variant}
+                />
+              </div>
+            ))
+          : null}
       </summary>
       <div
         onClick={(e) => {
--- src/types/ComponentTypes/LayoutTypes/collapsibleTypes.ts
+++ src/types/ComponentTypes/LayoutTypes/collapsibleTypes.ts
@@ -1,5 +1,8 @@
+import { ButtonType } from "../FormTypes";
+
 export interface CollapsibleType {
   label: string;
   children: JSX.Element | JSX.Element[] | null;
   initialOpen?: boolean;
+  actions?: ButtonType[];
 }
