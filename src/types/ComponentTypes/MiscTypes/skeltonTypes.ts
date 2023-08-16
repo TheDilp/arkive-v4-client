5f4e03666eb900a2f7f263d462c6398c7af7f016
= <=>
2023-08-16 10:54:13 +0200

    feat: skeleton row count for table

--- src/types/ComponentTypes/MiscTypes/skeltonTypes.ts
+++ src/types/ComponentTypes/MiscTypes/skeltonTypes.ts
@@ -1,1 +1,1 @@
-export type SkeletonType = { type: "table" | "breadcrumbs" | "drawer_form" | "editor" };
+export type SkeletonType = { type: "table" | "breadcrumbs" | "drawer_form" | "editor"; limit?: number };
