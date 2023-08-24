a909d82f21aff7f668ba6ce19edddff18dd25792
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-08-24 08:29:27 +0200

    feat: disable save for map pin drawer

--- src/components/Overlay/DrawerContent/MapPinDrawer.tsx
+++ src/components/Overlay/DrawerContent/MapPinDrawer.tsx
@@ -7,10 +7,29 @@ import { useCreateSubEntity, useGetSubEntity, useHandleChange, useUpdateMapSubEn
 import { MapPinType } from "../../../types";
 import { drawerAtom, IconEnum } from "../../../utils";
 import { InsertMapPinSchema, InsertMapPinType, UpdateMapPinSchema, UpdateMapPinType } from "../../../validation/maps/map_pins";
-import { ImagePreview } from "../../DataDisplay";
+import { CharacterPreview, ImagePreview } from "../../DataDisplay";
 import { Button, Checkbox, Input, Search } from "../../Form";
+import { Tabs } from "../../Layout";
 import { Skeleton } from "../../Misc";
 import { ColorPicker, IconPicker } from "..";
+
+function isSaveDisabled(mapPin: Partial<MapPinType>) {
+  if (!mapPin.icon && !mapPin.image_id) return true;
+  return false;
+}
+
+const tabs = [
+  {
+    id: "1",
+    label: "Basic info",
+    icon: IconEnum.info_circle,
+  },
+  {
+    id: "2",
+    label: "Characters",
+    icon: IconEnum.character,
+  },
+];
 
 type Props = {
   data: { id?: string; lat: number; lng: number };
@@ -18,6 +37,7 @@ type Props = {
 
 export function MapPinDrawer({ data }: Props) {
   const { item_id } = useParams();
+  const [selectedTab, setSelectedTab] = useState(0);
   const [mapPin, setMapPin] = useState<Partial<MapPinType>>({
     parent_id: item_id as string,
   });
@@ -25,7 +45,6 @@ export function MapPinDrawer({ data }: Props) {
   const { data: existingMapPin, isFetching } = useGetSubEntity(data?.id, "map_pins", { data: {} }, { enabled: !!data?.id });
   const { mutateAsync: createMapPin } = useCreateSubEntity<InsertMapPinType>("map_pins");
   const { mutateAsync: updateMapPin } = useUpdateMapSubEntity<UpdateMapPinType>("map_pins", item_id as string);
-
   const queryClient = useQueryClient();
 
   const { handleChange } = useHandleChange({ data: mapPin, setData: setMapPin });
@@ -50,57 +69,93 @@ export function MapPinDrawer({ data }: Props) {
 
   return (
     <div className="flex flex-col gap-y-2">
-      <div className="flex flex-nowrap gap-x-2">
-        <Input label="Map pin title (optional)" name="title" onChange={handleChange} value={mapPin?.title || ""} />
+      <Tabs onChange={(_, index) => setSelectedTab(index)} selectedTab={selectedTab} tabs={tabs} />
+      {selectedTab === 0 ? (
+        <>
+          <div className="flex flex-nowrap gap-x-2">
+            <Input label="Map pin title (optional)" name="title" onChange={handleChange} value={mapPin?.title || ""} />
 
-        <div className="flex flex-col justify-between">
-          <span className="block min-h-[20px] truncate text-center text-sm text-zinc-300">Icon</span>
-          <div className="flex items-center gap-x-2 pb-2">
-            <ColorPicker hasCustom name="color" onChange={handleChange} value={mapPin.color as string} />
-            <IconPicker icon={mapPin.icon || ""} iconColor={mapPin.color || "#ffffff"} name="icon" onChange={handleChange} />
+            <div className="flex flex-col justify-between">
+              <span className="block min-h-[20px] truncate text-center text-sm text-zinc-300">Icon</span>
+              <div className="flex items-center gap-x-2 pb-2">
+                <ColorPicker hasCustom name="color" onChange={handleChange} value={mapPin.color as string} />
+                <IconPicker
+                  icon={mapPin.icon || ""}
+                  iconColor={mapPin.color || "#ffffff"}
+                  name="icon"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
           </div>
+          <div className="flex flex-nowrap justify-between">
+            <span className="block min-h-[20px] truncate">Marker border:</span>
+            <div className="flex items-center gap-x-2 pb-2">
+              <ColorPicker hasCustom name="border_color" onChange={handleChange} value={mapPin.border_color as string} />
+              <Checkbox name="show_border" onChange={handleChange} value={mapPin?.show_border} />
+            </div>
+          </div>
+          <div className="flex flex-nowrap justify-between">
+            <span className="block min-h-[20px] truncate">Marker background:</span>
+            <div className="flex items-center gap-x-2 pb-2">
+              <ColorPicker
+                hasCustom
+                name="background_color"
+                onChange={handleChange}
+                value={mapPin.background_color as string}
+              />
+              <Checkbox name="show_background" onChange={handleChange} value={mapPin?.show_background} />
+            </div>
+          </div>
+          <div className="flex flex-nowrap justify-between">
+            <span className="block min-h-[20px] truncate">Public:</span>
+            <div className="flex items-center gap-x-2 pb-2">
+              <Checkbox name="is_public" onChange={handleChange} value={!!mapPin?.is_public} />
+            </div>
+          </div>
+          <div className="w-full">
+            {!mapPin?.image_id ? (
+              <Search
+                imageType="images"
+                label="Image (replaces icon if selected)"
+                name="image_id"
+                onChange={handleChange}
+                searchEntity="images"
+                value={mapPin.image_id || ""}
+              />
+            ) : (
+              <ImagePreview
+                clearAction={() => handleChange({ name: "image_id", value: null })}
+                id={mapPin?.image_id}
+                title={mapPin?.image?.title || ""}
+              />
+            )}
+          </div>
+        </>
+      ) : null}
+      {selectedTab === 1 ? (
+        <div className="flex flex-wrap gap-2">
+          {mapPin?.characters?.length
+            ? mapPin.characters.map((character) => (
+                <div key={character.id} className="w-full">
+                  <CharacterPreview
+                    character_name={character?.first_name}
+                    clearAction={() => {
+                      handleChange({
+                        name: "characters",
+                        value: (mapPin?.characters || []).filter((char) => char.id !== character.id),
+                      });
+                    }}
+                    id={character.id}
+                  />
+                </div>
+              ))
+            : null}
         </div>
-      </div>
-      <div className="flex flex-nowrap justify-between">
-        <span className="block min-h-[20px] truncate">Marker border:</span>
-        <div className="flex items-center gap-x-2 pb-2">
-          <ColorPicker hasCustom name="border_color" onChange={handleChange} value={mapPin.border_color as string} />
-          <Checkbox name="show_border" onChange={handleChange} value={mapPin?.show_border} />
-        </div>
-      </div>
-      <div className="flex flex-nowrap justify-between">
-        <span className="block min-h-[20px] truncate">Marker background:</span>
-        <div className="flex items-center gap-x-2 pb-2">
-          <ColorPicker hasCustom name="background_color" onChange={handleChange} value={mapPin.background_color as string} />
-          <Checkbox name="show_background" onChange={handleChange} value={mapPin?.show_background} />
-        </div>
-      </div>
-      <div className="flex flex-nowrap justify-between">
-        <span className="block min-h-[20px] truncate">Public:</span>
-        <div className="flex items-center gap-x-2 pb-2">
-          <Checkbox name="is_public" onChange={handleChange} value={!!mapPin?.is_public} />
-        </div>
-      </div>
-      <div className="w-full">
-        {!mapPin?.image_id ? (
-          <Search
-            imageType="images"
-            label="Image (replaces icon if selected)"
-            name="image_id"
-            onChange={handleChange}
-            searchEntity="images"
-            value={mapPin.image_id || ""}
-          />
-        ) : (
-          <ImagePreview
-            clearAction={() => handleChange({ name: "image_id", value: null })}
-            id={mapPin?.image_id}
-            title={mapPin?.image?.title || ""}
-          />
-        )}
-      </div>
+      ) : null}
       <Button
         icon={IconEnum.save}
+        isDisabled={isSaveDisabled(mapPin)}
         label="Save"
         onClick={async () => {
           if (!("id" in data) || !data?.id) {
