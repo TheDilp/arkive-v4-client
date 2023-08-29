ce3577091717a89e1371208a7fdb7e934615b4dc
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-08-29 13:06:46 +0200

    fix: project card overflow

--- src/components/Layout/Card.tsx
+++ src/components/Layout/Card.tsx
@@ -9,7 +9,7 @@ export function ProjectCard({ id, title, image }: ProjectCardType) {
   const navigate = useNavigate();
   return (
     <Link
-      className="group relative col-span-1 flex h-[28rem] flex-col items-center justify-center overflow-hidden rounded bg-zinc-950 bg-cover bg-center bg-no-repeat shadow transition-all animate-in fade-in duration-500"
+      className="group relative col-span-1 flex h-[28rem] flex-col items-center justify-center rounded bg-zinc-950 bg-cover bg-center bg-no-repeat shadow transition-all animate-in fade-in duration-500"
       to={`/projects/${id}`}>
       <h2 className="absolute top-[20%] z-10 max-w-full select-none truncate px-4 text-center font-merriweather text-4xl font-semibold text-white drop-shadow transition-all ">
         {title}
