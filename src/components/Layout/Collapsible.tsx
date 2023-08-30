27b7419307dbed27c399d495eabd8467d30d47bb
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-08-30 10:02:07 +0200

    fix: collapsible cursor

--- src/components/Layout/Collapsible.tsx
+++ src/components/Layout/Collapsible.tsx
@@ -6,8 +6,8 @@ import { Button, Icon } from "..";
 
 export function Collapsible({ label, initialOpen, children, actions }: CollapsibleType) {
   return (
-    <details className="select-none" open={initialOpen}>
-      <summary className="flex items-center gap-x-2 border-b border-zinc-700 pb-1 ">
+    <details className="cursor-default select-none" open={initialOpen}>
+      <summary className="flex cursor-pointer items-center gap-x-2 border-b border-zinc-700 pb-1">
         <span className="select-none text-xl">{label}</span>
         <span className="ml-auto">
           <Icon fontSize={28} icon={IconEnum.chevron_up} />
