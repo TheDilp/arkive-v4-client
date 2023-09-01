b8c3f6a0303a2f81d2c8ca0c2f7caace56646605
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-01 14:58:42 +0200

    fix: char card

--- src/components/Layout/Card.tsx
+++ src/components/Layout/Card.tsx
@@ -79,7 +79,13 @@ export function ProjectDashboardInfoCard({ title, count, icon, latestItems }: Pr
   );
 }
 
-export function CharacterCard({ id, first_name, last_name, portrait, is_favorite }: CharacterType) {
+export function CharacterCard({
+  id,
+  first_name,
+  last_name,
+  portrait_id,
+  is_favorite,
+}: Pick<CharacterType, "id" | "first_name" | "last_name" | "portrait_id" | "is_favorite">) {
   const { project_id } = useParams();
   return (
     <Link
@@ -96,7 +102,7 @@ export function CharacterCard({ id, first_name, last_name, portrait, is_favorite
       <div
         className="absolute z-0 flex h-full w-full flex-col items-center justify-end bg-zinc-950 bg-cover bg-top transition-all duration-300 group-hover:scale-125 group-hover:brightness-75 lg:bg-center"
         style={{
-          backgroundImage: portrait?.id ? `url(${getImageURL(project_id as string, "images", portrait?.id)})` : "",
+          backgroundImage: portrait_id ? `url(${getImageURL(project_id as string, "images", portrait_id)})` : "",
         }}
       />
     </Link>
