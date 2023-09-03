57a585c077374e8ba84ef75613829a77f826130a
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-03 20:34:18 +0200

    fix: active sidebar item position

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -13,13 +13,13 @@ const SidebarClasses = tv({
     base: "flex w-full max-w-full flex-col border-r border-zinc-800 bg-zinc-900 lg:w-16 lg:min-w-[4rem] lg:max-w-[4rem] h-16 lg:h-full sticky bottom-0 lg:relative z-10",
     nav: "flex h-48 flex-1 flex-col overflow-x-hidden overflow-y-hidden lg:h-full lg:max-w-[4rem] lg:overflow-y-auto lg:overflow-x-hidden",
     list: "flex w-screen overflow-x-auto lg:w-full scrollbar-hidden lg:flex-1 lg:flex-col lg:items-center lg:justify-start lg:overflow-x-hidden",
-    sidebarLogo: "flex h-16 items-center w-16 min-w-[4rem] sticky top-0 left-0 bg-zinc-900",
+    sidebarLogo: "flex h-16 items-center justify-center w-16 min-w-[4rem] min-h-[4rem] sticky top-0 left-0 bg-zinc-900",
     listItemLink: "w-full cursor-pointer transition-all lg:mx-0 flex justify-center w-16",
     listItem:
-      "flex h-16 max-w-[4rem] min-w-[4rem] items-center justify-center transition-colors hover:text-blue-400 justify-center",
-    listSettingsItem: "justify-center flex h-16 min-w-[4rem] items-center lg:mx-0 lg:ml-0 lg:mt-auto",
+      "flex h-16 max-w-[4rem] min-w-[4rem] min-h-[4rem] items-center justify-center transition-colors hover:text-blue-400 justify-center",
+    listSettingsItem: "justify-center flex h-16 min-w-[4rem] min-h-[4rem] items-center lg:mx-0 lg:ml-0 lg:mt-auto",
     settingsSubitem: "flex items-center gap-x-2",
-    activeSettingsSubItem: "text-blue-400",
+    activeSettingsSubItem: "text-blue-400 min-h-[4rem]",
     navIcon: "cursor-pointer hover:text-blue-300 text-white",
   },
 });
