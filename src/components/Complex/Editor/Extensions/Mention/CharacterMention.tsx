749f80b39240f16eae67a7da149f81c9ca88c0e8
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-17 11:54:50 +0200

    feat: character mention

---
+++ src/components/Complex/Editor/Extensions/Mention/CharacterMention.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+
+import { IconEnum } from "../../../../../utils";
+
+type Props = {
+  title?: string;
+  nodeId: string | undefined;
+  nodeLabel: string;
+  project_id: string | undefined;
+};
+export function CharacterMention({ nodeId, project_id, title, nodeLabel }: Props) {
+  return nodeId ? (
+    <Link
+      className="font-Lato inline-flex items-center text-sm font-bold text-white underline transition-colors hover:text-sky-400"
+      to={!project_id ? `/view/characters/${nodeId}` : `/projects/${project_id}/characters/${nodeId}`}>
+      <Icon fontSize={15} icon={IconEnum.character} />
+      {title || nodeLabel}
+    </Link>
+  ) : (
+    <div className="Lato text-white">{nodeLabel}</div>
+  );
+}
--- src/components/Complex/Editor/Extensions/Mention/MentionReactComponent.tsx
+++ src/components/Complex/Editor/Extensions/Mention/MentionReactComponent.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 
 import { DocumentMention, GraphMention, MapMention, WordMention } from ".";
+import { CharacterMention } from "./CharacterMention";
 
 type Props = {
   node: any;
@@ -10,8 +11,9 @@ export function MentionReactComponent({ node }: Props) {
   const { project_id } = useParams();
 
   if (node?.attrs) {
-    const { id, name, label, alterId } = node.attrs;
+    const { id, name, label, alterId } = node.attrs as { id: string; name: string; label: string; alterId?: string };
 
+    if (name === "characters") return <CharacterMention nodeId={id} nodeLabel={label} project_id={project_id} />;
     if (name === "documents")
       return <DocumentMention alterId={alterId} id={id} label={label} project_id={project_id} title={label} />;
     if (name === "maps") return <MapMention nodeId={id} nodeLabel={label} project_id={project_id} />;
