d4ac3255aa32fa60f75bdaad4f9580d40ebd0eb5
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-19 13:14:27 +0200

    chore: update char drawer

--- src/components/Overlay/DrawerContent/CharacterDrawer.tsx
+++ src/components/Overlay/DrawerContent/CharacterDrawer.tsx
@@ -314,7 +314,7 @@ function RelationshipRow({
   return (
     <li className="flex items-center gap-x-2">
       <div className="flex flex-1 items-center gap-x-2">
-        <ItemPreview title={character_name} id={id} type="characters" image_id={portrait_id} />
+        <ItemPreview id={id} image_id={portrait_id} title={character_name} type="characters" />
         <span className="truncate whitespace-nowrap">is {current_character_first_name || "this character"}&apos;s </span>
       </div>
       <div className="max-w-[10rem] flex-1">
