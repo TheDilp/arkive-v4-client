84d0f62f10579a93ed39ffbe6a41571ef8bc31b4
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-19 13:05:38 +0200

    feat: item preview

--- src/components/DataDisplay/ItemPreview.tsx
+++ src/components/DataDisplay/ItemPreview.tsx
@@ -1,26 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import { ItemPreviewType } from "../../types/ComponentTypes/DataDisplayTypes/itemPreviewTypes";
-import { IconEnum } from "../../utils";
+import { getDefaultEntityIcon, getImageURL, IconEnum } from "../../utils";
 import { Button } from "../Form";
-import { Icon } from "../Misc";
+import { Avatar, Icon } from "../Misc";
 
-export function ItemPreview({ id, title, icon, link, clearAction }: ItemPreviewType) {
+export function ItemPreview({ id, title, type, link, image_id, label, clearAction }: ItemPreviewType) {
+  const { project_id } = useParams();
   return (
-    <Link
-      className={`flex items-center gap-x-2 rounded bg-zinc-700 p-2 ${link ? "transition-all hover:text-blue-400" : ""}`}
-      to={link ?? "#"}>
-      {icon ? (
-        <span className="">
-          <Icon fontSize={20} icon={icon} />
-        </span>
-      ) : null}
-      <span>{title}</span>
-      {clearAction ? (
-        <span className="ml-auto w-min">
-          <Button hasNoBackground icon={IconEnum.close} isIconOnly onClick={() => clearAction(id)} />
-        </span>
-      ) : null}
-    </Link>
+    <div className="flex flex-col gap-y-2">
+      {label ? <div className="block min-h-[20px] truncate text-sm text-zinc-300">{label}</div> : null}
+      <Link
+        className={`flex items-center gap-x-2 rounded bg-zinc-700 p-2 ${link ? "transition-all hover:text-blue-400" : ""}`}
+        to={link ?? "#"}>
+        {type !== "characters" ? (
+          <span className="">
+            <Icon fontSize={20} icon={getDefaultEntityIcon(type)} />
+          </span>
+        ) : (
+          <Avatar image={getImageURL(project_id as string, "images", image_id)} label={title} size="sm" />
+        )}
+        <span>{title}</span>
+        {clearAction ? (
+          <span className="ml-auto w-min">
+            <Button
+              hasNoBackground
+              icon={IconEnum.close}
+              isIconOnly
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                clearAction(id);
+              }}
+            />
+          </span>
+        ) : null}
+      </Link>
+    </div>
   );
 }
--- src/types/ComponentTypes/DataDisplayTypes/itemPreviewTypes.ts
+++ src/types/ComponentTypes/DataDisplayTypes/itemPreviewTypes.ts
@@ -1,7 +1,12 @@
+import { AvailableEntityType, AvailableSubEntityType } from "../../EntityTypes";
+
 export interface ItemPreviewType {
   id: string;
   title: string;
   icon?: string;
   link?: string;
+  label?: string;
+  image_id?: string;
   clearAction?: (id: string) => void;
+  type: AvailableEntityType | AvailableSubEntityType;
 }
