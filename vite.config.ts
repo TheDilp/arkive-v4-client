2d0dc71962d2333def9c6f1f6c4458257d5f61f7
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-19 15:04:11 +0200

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   plugins: [
     react({
       babel: {
