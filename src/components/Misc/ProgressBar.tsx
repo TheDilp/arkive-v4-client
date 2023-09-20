7cc6d149411d9e27368db324742cf83abcc58ffc
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-20 15:25:33 +0200

    chore: change progress color

--- src/components/Misc/ProgressBar.tsx
+++ src/components/Misc/ProgressBar.tsx
@@ -1,7 +1,7 @@
 export function IndeterminateProgressBar() {
   return (
     <div className="absolute top-[3.825rem] w-full">
-      <div className="h-0.5 w-full overflow-hidden bg-green-500">
+      <div className="h-0.5 w-full overflow-hidden bg-green-600">
         <div className="h-full w-full origin-left-right animate-progress bg-green-700" />
       </div>
     </div>
