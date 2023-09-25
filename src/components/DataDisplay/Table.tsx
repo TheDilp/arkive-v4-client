edf67ae1d865e4ceace97c9b61443d6da5cc28a3
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-25 16:33:42 +0200

    fix: pagination z index

--- src/components/DataDisplay/Table.tsx
+++ src/components/DataDisplay/Table.tsx
@@ -63,7 +63,7 @@ const TableClasses = tv({
     content: "flex flex-1 items-center truncate px-2 box-border border-zinc-600 border-r last:border-r-0",
     centeredContent: "flex items-center justify-center",
     paginationContainer:
-      "flex lg:h-10 h-8 max-h-8 lg:max-h-10 items-start justify-between border-zinc-600 pl-2 pt-0.5 sticky bottom-0 bg-zinc-950 pb-9 pt-1",
+      "flex lg:h-10 h-8 max-h-8 lg:max-h-10 items-start justify-between border-zinc-600 pl-2 pt-0.5 z-50 sticky bottom-0 bg-zinc-950 pb-9 pt-1",
     pageCountContainer: "font-lato flex flex-nowrap items-center gap-x-2",
     pageCount: "w-max",
     showPageCount: "flex flex-nowrap items-center gap-x-2",
