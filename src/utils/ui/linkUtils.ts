42515ef8c80829ffb01b2a8b45931545191fa719
Arctur <mihailo.dilparic@gmail.com>
2023-10-25 15:13:16 +0200

    fix: search link

--- src/utils/ui/linkUtils.ts
+++ src/utils/ui/linkUtils.ts
@@ -1,12 +1,14 @@
 export function getSearchLink(project_id: string, name: string, item_id: string, parent_id?: string) {
   if (name === "") return "#";
+  if (name === "characters") {
+    return `/projects/${project_id}/characters/${parent_id || item_id}${parent_id ? `/${item_id}` : ""}/resources`;
+  }
   let link_type = "";
   if (name === "alter_names") link_type = "documents";
   if (name === "map_pins" || name === "character_map_pins") link_type = "maps";
   if (name === "nodes" || name === "edges" || name === "boards") link_type = "graphs";
   if (name === "events") link_type = "calendars";
-  if (name === "characters" || name === "documents" || name === "maps" || name === "calendars" || name === "dictionaries")
-    link_type = name;
+  if (name === "documents" || name === "maps" || name === "calendars" || name === "dictionaries") link_type = name;
   if (link_type) return `/projects/${project_id}/${link_type}/${parent_id || item_id}${parent_id ? `/${item_id}` : ""}`;
 
   return "#";
