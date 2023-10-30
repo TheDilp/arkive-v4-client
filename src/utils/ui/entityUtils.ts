1414f84927978e0ad0ce006b87e11ed32c543acf
Arctur <mihailo.dilparic@gmail.com>
2023-10-30 07:55:40 +0100

    chore: adjust blueprint column widths

--- src/utils/ui/entityUtils.ts
+++ src/utils/ui/entityUtils.ts
@@ -94,11 +94,12 @@ export function getSearchFieldTypeLinkType(type: string) {
   return "";
 }
 
-export function getBlueprintInstanceColumnWidth(type: BlueprintFieldTypes): { minSize: number; maxSize: number } {
-  if (type === "boolean" || type === "images_single" || type === "characters_single" || type === "number")
-    return { minSize: 5, maxSize: 5 };
-  if (type === "text" || type === "characters_multiple" || type === "select" || type === "dice_roll")
+export function getBlueprintInstanceColumnWidth(type: BlueprintFieldTypes): { minSize: number; maxSize?: number } {
+  if (type === "text") return { minSize: 10, maxSize: 25 };
+  if (type === "images_single" || type === "characters_single" || type === "number") return { minSize: 7.5, maxSize: 7.5 };
+  if (type === "characters_multiple" || type === "select" || type === "dice_roll" || type === "random_table")
     return { minSize: 10, maxSize: 15 };
-  if (type === "select_multiple" || type === "random_table") return { minSize: 10, maxSize: 15 };
+  if (type === "boolean") return { minSize: 2.75, maxSize: 2.75 };
+  if (type === "select_multiple") return { minSize: 10, maxSize: 35 };
   return { minSize: 10, maxSize: 10 };
 }
