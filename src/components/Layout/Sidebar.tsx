6fb1407a2fa287fe5b4fe81f99f991850614c38a
Arctur <mihailo.dilparic@gmail.com>
2023-10-30 20:01:01 +0100

    fix: sidebar

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -12,17 +12,15 @@ const SidebarClasses = tv({
   slots: {
     base: "flex w-full max-w-full flex-col border-r border-zinc-800 bg-zinc-900 lg:w-16 lg:min-w-[4rem] lg:max-w-[4rem] h-16 lg:h-full sticky bottom-0 lg:relative z-50",
     nav: "flex h-48 flex-1 flex-col overflow-x-hidden overflow-y-hidden lg:h-full lg:max-w-[4rem] lg:overflow-y-auto lg:overflow-x-hidden",
-    list: "flex w-screen overflow-x-auto lg:w-full lg:flex-1 lg:flex-col lg:items-center lg:justify-start lg:overflow-x-hidden",
+    list: "flex w-screen overflow-x-auto lg:w-full lg:flex-1 lg:flex-col lg:items-center lg:justify-start lg:overflow-x-hidden lg:overflow-y-auto overflow-y-hidden",
     sidebarLogo:
       "flex h-16 items-center justify-center w-16 min-w-[4rem] min-h-[4rem] sticky top-0 left-0 bg-zinc-900 select-none",
     listItemLink: "w-full cursor-pointer transition-all lg:mx-0 flex justify-center w-16 hover:fill-blue-300",
     listItem:
       "flex h-16 max-w-[4rem] min-w-[4rem] min-h-[4rem] items-center justify-center transition-colors hover:text-blue-400 justify-center",
+    selectedListItem: "text-white sticky left-16 lg:top-16 lg:bottom-16 right-16 bg-blue-400 [&>li]:hover:text-white",
     listSettingsItem:
-      "bottom-0 right-0 sticky justify-center flex h-16 min-w-[4rem] min-h-[4rem] items-center lg:mx-0 lg:ml-0 lg:mt-auto bg-zinc-900",
-    selectedListItem: "text-white sticky left-16 top-16 bottom-16 right-16 bg-blue-400 [&>li]:hover:text-white",
-    settingsSubitem: "flex items-center gap-x-2 select-none",
-    activeSettingsSubItem: "text-blue-400",
+      "lg:-bottom-0 -right-0 sticky justify-center flex h-16 min-w-[4rem] min-h-[4rem] items-center lg:mx-0 lg:ml-0 lg:mt-auto bg-zinc-900",
     navIcon: "cursor-pointer",
   },
 });
