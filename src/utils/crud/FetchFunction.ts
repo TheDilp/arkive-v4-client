8493e7ca93979ee0a9d3ea4585951a0e7c849f9d
Arctur <mihailo.dilparic@gmail.com>
2023-10-30 23:11:25 +0100

    feat: add authorization to requests

--- src/utils/crud/FetchFunction.ts
+++ src/utils/crud/FetchFunction.ts
@@ -1,5 +1,5 @@
 export function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
+  const value = `; ${document?.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()!.split(";").shift();
   return "";
@@ -14,10 +14,12 @@ export async function FetchFunction({
   method: "GET" | "POST" | "DELETE";
   body?: string | FormData;
 }) {
+  const token = getCookie("__session");
   const res = await fetch(url, {
     method,
     body,
     headers: {
+      Authorization: `Bearer ${token}`,
       "Access-Control-Allow-Origin": "*",
       ...(typeof body === "string" ? { "Content-Type": "application/json" } : {}),
     },
@@ -25,6 +27,10 @@ export async function FetchFunction({
 
   const data = await res.json();
   if (!data.ok) {
+    if (data.message === "UNAUTHORIZED" && res.status === 403) {
+      // @ts-ignore
+      window?.Clerk?.signOut();
+    }
     throw new Error("There was an error with your request.");
   }
   return data;
