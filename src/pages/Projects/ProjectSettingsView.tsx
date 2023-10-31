224750f1f488d929b84ae757b1ac1118c3043a7d
Arctur <mihailo.dilparic@gmail.com>
2023-10-31 13:49:32 +0100

    fix: settings page layout

--- src/pages/Projects/ProjectSettingsView.tsx
+++ src/pages/Projects/ProjectSettingsView.tsx
@@ -163,161 +163,112 @@ export function ProjectSettingsView() {
   }
 
   return (
-    <div className="grid h-full max-h-full w-full grid-cols-5 content-start gap-4 overflow-hidden pt-0 lg:content-stretch">
-      <div className="col-span-5 flex h-full min-h-fit flex-col items-center gap-y-2 overflow-hidden rounded-lg bg-zinc-800 lg:col-span-1 lg:h-full lg:max-h-full">
-        <div className="w-full">
-          {isLoading ? <Skeleton type="character_profile" /> : null}
-          {!isLoading && isLg ? (
-            <div className="flex flex-col items-center gap-y-2 rounded-lg bg-zinc-800 p-4 lg:col-span-1">
-              <Avatar
-                hasShowImage
-                image={getImageURL(projectData?.data?.id as string, "images", projectData?.data?.image_id)}
-                initials={getFirstLetters(projectData?.data?.title as string)}
-                isTooltipDisabled
-                size="4xl"
-              />
+    <div className="flex h-full min-h-full flex-col gap-y-2">
+      <div className="w-full flex-1 content-start gap-4 pt-0 lg:grid lg:grid-cols-5 lg:content-stretch">
+        {isLoading ? <Skeleton type="character_profile" /> : null}
+        {!isLoading && isLg ? (
+          <div className="flex flex-col items-center gap-y-2 rounded-lg bg-zinc-800 p-4 lg:col-span-1">
+            <Avatar
+              hasShowImage
+              image={getImageURL(projectData?.data?.id as string, "images", projectData?.data?.image_id)}
+              initials={getFirstLetters(projectData?.data?.title as string)}
+              isTooltipDisabled
+              size="4xl"
+            />
 
-              <div className="mt-2 flex flex-col gap-y-1">
-                <h2 className="text-center font-merriweather text-lg">{`${projectData?.data?.title || ""}`.trimEnd()}</h2>
-              </div>
-
-              <div className="w-full">
-                <Tabs isVertical onChange={(_, index) => setSelectedTab(index)} selectedTab={selectedTab} tabs={tabs} />
-              </div>
+            <div className="mt-2 flex flex-col gap-y-1">
+              <h2 className="text-center font-merriweather text-lg">{`${projectData?.data?.title || ""}`.trimEnd()}</h2>
             </div>
-          ) : null}
-          {!isLoading && !isLg ? (
+
             <div className="w-full">
-              <Tabs onChange={(_, index) => setSelectedTab(index)} selectedTab={selectedTab} tabs={tabs} />
+              <Tabs isVertical onChange={(_, index) => setSelectedTab(index)} selectedTab={selectedTab} tabs={tabs} />
             </div>
-          ) : null}
-        </div>
-      </div>
+          </div>
+        ) : null}
+        {!isLoading && !isLg ? (
+          <div className="w-full">
+            <Tabs onChange={(_, index) => setSelectedTab(index)} selectedTab={selectedTab} tabs={tabs} />
+          </div>
+        ) : null}
 
-      <div className="col-span-5 min-h-[calc(100%)] rounded-lg bg-zinc-950 p-4 lg:col-span-4">
-        <h2 className="mb-4 flex h-8 items-center border-b border-zinc-900 pb-2 font-merriweather text-2xl">
-          {tabs[selectedTab].label}
+        <div className="flex h-[calc(100vh-12rem)] max-h-[calc(100vh-12rem)] flex-1 flex-col overflow-hidden rounded-lg bg-zinc-950 p-4 lg:col-span-4 lg:h-[calc(100vh-6rem)] lg:max-h-[calc(100vh-6rem)]">
+          <h2 className="mb-4 flex h-8 items-center border-b border-zinc-900 pb-2 font-merriweather text-2xl">
+            {tabs[selectedTab].label}
+            {selectedTab === 0 ? (
+              <div className="ml-auto w-min">
+                <Button
+                  icon={IconEnum.save}
+                  isDisabled={isUpdating}
+                  isLoading={isUpdating}
+                  label="Save"
+                  onClick={handleSave}
+                  size="sm"
+                  variant="success"
+                />
+              </div>
+            ) : null}
+            {selectedTab === 1 ? (
+              <div className="ml-auto w-min">
+                <Button
+                  icon={IconEnum.add}
+                  label="Create"
+                  onClick={handleOpenNewRelationshipTypeDrawer}
+                  size="sm"
+                  variant="info"
+                />
+              </div>
+            ) : null}
+            {selectedTab === 3 ? (
+              <div className="ml-auto w-min">
+                <Button
+                  icon={IconEnum.user_invite}
+                  label="Invite"
+                  onClick={handleOpenInviteToProjectDrawer}
+                  size="sm"
+                  variant="info"
+                />
+              </div>
+            ) : null}
+          </h2>
           {selectedTab === 0 ? (
-            <div className="ml-auto w-min">
-              <Button
-                icon={IconEnum.save}
-                isDisabled={isUpdating}
-                isLoading={isUpdating}
-                label="Save"
-                onClick={handleSave}
-                size="sm"
-                variant="success"
-              />
-            </div>
-          ) : null}
-          {selectedTab === 1 ? (
-            <div className="ml-auto w-min">
-              <Button
-                icon={IconEnum.add}
-                label="Create"
-                onClick={handleOpenNewRelationshipTypeDrawer}
-                size="sm"
-                variant="info"
-              />
-            </div>
-          ) : null}
-          {selectedTab === 3 ? (
-            <div className="ml-auto w-min">
-              <Button
-                icon={IconEnum.user_invite}
-                label="Invite"
-                onClick={handleOpenInviteToProjectDrawer}
-                size="sm"
-                variant="info"
-              />
-            </div>
-          ) : null}
-        </h2>
-        {selectedTab === 0 ? (
-          <div className="flex h-full max-h-[calc(100%-3rem)] flex-col gap-y-4 overflow-auto">
-            <div className=" grid grid-cols-12 gap-2">
-              <div className="col-span-12 lg:col-span-9">
-                <Input label="Title" name="title" onChange={handleChange} value={project?.title || ""} />
-              </div>
-              <div className="col-span-12 lg:col-span-3">
-                <ImageSelect
-                  isIconOnly={isLg}
-                  label="Project image"
-                  name="image_id"
-                  onChange={handleChange}
-                  type="images"
-                  value={project?.image_id || ""}
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-y-2">
-              <div className="flex flex-nowrap items-center justify-between border-zinc-700">
-                <span>Default dice color:</span>
-                <ColorPicker
-                  name="default_dice_color"
-                  onChange={handleChange}
-                  value={project?.default_dice_color || DefaultTagColor}
-                />
-              </div>
-              <div className="flex flex-nowrap items-center justify-between border-t border-zinc-700 pt-2">
-                <span>Show images in folder view:</span>
-                <Checkbox name="show_image_folder_view" onChange={handleChange} value={false} />
-              </div>
-              <div className="flex flex-nowrap items-center justify-between border-t border-zinc-700 pt-2">
-                <span>Show images in table folder view:</span>
-                <Checkbox name="show_image_table_view" onChange={handleChange} value={false} />
-              </div>
-            </div>
-          </div>
-        ) : null}
-        {selectedTab === 1 ? (
-          <div>
-            <TablePageLayout>
-              <Table
-                columns={relationshipTableColumns(setDialog)}
-                data={projectData?.data?.character_relationship_types || []}
-                dispatch={dispatch}
-                type="character_relationship_types"
-              />
-            </TablePageLayout>
-          </div>
-        ) : null}
-        {selectedTab === 2 ? (
-          <div className="flex max-h-[94.75%] flex-col gap-y-0 overflow-y-auto pb-6">
-            <Title label="Notifications from other project members" size="xl" />
-            {AllEntities.map((entity) => (
-              <div key={entity} className="flex flex-nowrap items-center justify-between border-t border-zinc-700 py-1 pt-0">
-                <span>{capitalizeFirstLetter(getSentenceCase(entity))}:</span>
-                <div className="flex w-52 items-center justify-between gap-x-2 text-center">
-                  <Checkbox
-                    isDisabled
-                    label="Create"
-                    name={`${entity}_create_notification`}
-                    onChange={() => {}}
-                    value={false}
-                  />
-                  <Checkbox
-                    isDisabled
-                    label="Update"
-                    name={`${entity}_update_notification`}
-                    onChange={() => {}}
-                    value={false}
-                  />
-                  <Checkbox
-                    isDisabled
-                    label="Delete"
-                    name={`${entity}_delete_notification`}
-                    onChange={() => {}}
-                    value={false}
+            <div className="flex h-full max-h-[calc(100%-3rem)] flex-col gap-y-4 overflow-auto">
+              <div className=" grid grid-cols-12 gap-2">
+                <div className="col-span-12 lg:col-span-9">
+                  <Input label="Title" name="title" onChange={handleChange} value={project?.title || ""} />
+                </div>
+                <div className="col-span-12 lg:col-span-3">
+                  <ImageSelect
+                    isIconOnly={isLg}
+                    label="Project image"
+                    name="image_id"
+                    onChange={handleChange}
+                    type="images"
+                    value={project?.image_id || ""}
                   />
                 </div>
               </div>
-            ))}
-          </div>
-        ) : null}
-        {selectedTab === 3 ? (
-          <div className="flex max-h-[94.75%] flex-col gap-y-0 overflow-y-auto pb-6">
+
+              <div className="flex flex-col gap-y-2">
+                <div className="flex flex-nowrap items-center justify-between border-zinc-700">
+                  <span>Default dice color:</span>
+                  <ColorPicker
+                    name="default_dice_color"
+                    onChange={handleChange}
+                    value={project?.default_dice_color || DefaultTagColor}
+                  />
+                </div>
+                <div className="flex flex-nowrap items-center justify-between border-t border-zinc-700 pt-2">
+                  <span>Show images in folder view:</span>
+                  <Checkbox name="show_image_folder_view" onChange={handleChange} value={false} />
+                </div>
+                <div className="flex flex-nowrap items-center justify-between border-t border-zinc-700 pt-2">
+                  <span>Show images in table folder view:</span>
+                  <Checkbox name="show_image_table_view" onChange={handleChange} value={false} />
+                </div>
+              </div>
+            </div>
+          ) : null}
+          {selectedTab === 1 ? (
             <div>
               <TablePageLayout>
                 <Table
@@ -328,8 +279,55 @@ export function ProjectSettingsView() {
                 />
               </TablePageLayout>
             </div>
-          </div>
-        ) : null}
+          ) : null}
+          {selectedTab === 2 ? (
+            <div className="flex max-h-[90%] flex-col gap-y-0 overflow-y-auto">
+              <Title label="Notifications from other project members" size="xl" />
+              {AllEntities.map((entity) => (
+                <div key={entity} className="flex flex-nowrap items-center justify-between border-t border-zinc-700 py-1 pt-0">
+                  <span>{capitalizeFirstLetter(getSentenceCase(entity))}:</span>
+                  <div className="flex w-52 items-center justify-between gap-x-2 text-center">
+                    <Checkbox
+                      isDisabled
+                      label="Create"
+                      name={`${entity}_create_notification`}
+                      onChange={() => {}}
+                      value={false}
+                    />
+                    <Checkbox
+                      isDisabled
+                      label="Update"
+                      name={`${entity}_update_notification`}
+                      onChange={() => {}}
+                      value={false}
+                    />
+                    <Checkbox
+                      isDisabled
+                      label="Delete"
+                      name={`${entity}_delete_notification`}
+                      onChange={() => {}}
+                      value={false}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : null}
+          {selectedTab === 3 ? (
+            <div className="flex max-h-[94.75%] flex-col gap-y-0 overflow-y-auto pb-6">
+              <div>
+                <TablePageLayout>
+                  <Table
+                    columns={relationshipTableColumns(setDialog)}
+                    data={projectData?.data?.character_relationship_types || []}
+                    dispatch={dispatch}
+                    type="character_relationship_types"
+                  />
+                </TablePageLayout>
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
