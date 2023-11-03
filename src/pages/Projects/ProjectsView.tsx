0bcc3339b1144dfcd2524d423f64578a8f3285d8
Arctur <mihailo.dilparic@gmail.com>
2023-11-03 17:47:36 +0100

    fix: projects view sidebar width and border

--- src/pages/Projects/ProjectsView.tsx
+++ src/pages/Projects/ProjectsView.tsx
@@ -16,7 +16,7 @@ export function ProjectsView() {
   return (
     <div className="flex h-screen w-screen">
       <Drawer />
-      <div className="h-full w-16 bg-zinc-900">
+      <div className="h-full w-16 min-w-[4rem] max-w-[4rem] border-r border-zinc-800 bg-zinc-900">
         <div className="flex h-16 w-full items-center justify-center">
           <img alt="Arkive Logo" className="h-12" src="/Logo.webp" />
         </div>
