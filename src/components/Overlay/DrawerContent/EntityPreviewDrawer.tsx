e2ab5e3a337fbcbee8c9b76acdddab642b3d0935
Arctur <mihailo.dilparic@gmail.com>
2023-11-03 10:15:52 +0100

    feat: doc preview drawer

--- src/components/Overlay/DrawerContent/EntityPreviewDrawer.tsx
+++ src/components/Overlay/DrawerContent/EntityPreviewDrawer.tsx
@@ -1,7 +1,9 @@
 import { useGetEntity } from "../../../hooks";
 import { MapView } from "../../../pages/Entities";
-import { AvailableEntityType, MapType } from "../../../types";
+import { AvailableEntityType, DocumentType, MapType } from "../../../types";
+import { Editor } from "../../Complex";
 import { DrawerLayout } from "../../Layout";
+import { Alert } from "../../Misc";
 
 export function MapPreviewDrawer({ id, subitem_id }: { id: string; subitem_id?: string }) {
   const { data: existingMap, isLoading } = useGetEntity<MapType>(
@@ -24,10 +26,34 @@ export function MapPreviewDrawer({ id, subitem_id }: { id: string; subitem_id?: 
   );
 }
 
+export function DocumentPreviewDrawer({ id }: { id: string }) {
+  const { data: existingDocument, isLoading } = useGetEntity<DocumentType>(
+    id,
+    "documents",
+    {
+      data: { id },
+    },
+    {
+      enabled: !!id,
+    },
+  );
+
+  if (isLoading) return null;
+  return (
+    <div className="h-full w-full overflow-hidden">
+      {existingDocument?.data?.content ? (
+        <Editor initialContent={existingDocument?.data?.content} isReadOnly name="editor" onChange={() => {}} />
+      ) : (
+        <Alert label="This document has no content." />
+      )}
+    </div>
+  );
+}
+
 export function EntityPreviewDrawer({ data }: { data: { id: string; subitem_id?: string; entity_type: AvailableEntityType } }) {
   return (
     <DrawerLayout>
-      {data.entity_type === "documents" ? <MapPreviewDrawer id={data.id} subitem_id={data?.subitem_id} /> : null}
+      {data.entity_type === "documents" ? <DocumentPreviewDrawer id={data.id} /> : null}
       {data.entity_type === "maps" ? <MapPreviewDrawer id={data.id} subitem_id={data?.subitem_id} /> : null}
       {data.entity_type === "graphs" ? <MapPreviewDrawer id={data.id} /> : null}
     </DrawerLayout>
