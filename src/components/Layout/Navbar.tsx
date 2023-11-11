b5cc36e3e93ce3d103887be415cfd7516b395214
Arctur <mihailo.dilparic@gmail.com>
2023-11-11 16:02:34 +0100

    fix: default navbar notif duration

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -114,7 +114,7 @@ export function Navbar() {
           title: lastJsonMessage.message,
           image_id: lastJsonMessage.image_id,
           variant: "info",
-          timer: 50,
+          timer: 5,
           image_url: lastJsonMessage.userImageUrl,
           hasNoTruncate: true,
         });
