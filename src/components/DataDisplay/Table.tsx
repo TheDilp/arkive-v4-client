67b7a9a12bf02e9ab1cb3e5e880c383423be7d87
Arctur <mihailo.dilparic@gmail.com>
2023-11-13 22:43:38 +0100

    fix: table sizing

--- src/components/DataDisplay/Table.tsx
+++ src/components/DataDisplay/Table.tsx
@@ -514,9 +514,10 @@ export function Table({ columns, data = [], config, isLoading, pagination, dispa
                 // 40px added to account for the subheader when it is enabled
                 rowVirtualizer.getTotalSize() +
                 Number(headerRef?.current?.clientHeight) +
-                (pagination ? 20 : 0) +
+                (pagination ? 28 : 0) +
                 (!hasNoHeaderGap && !pagination ? 20 : 0) +
-                (isSubheaderEnabled ? 40 : 0)
+                (isSubheaderEnabled && !pagination ? 40 : 0) +
+                (isSubheaderEnabled && pagination ? 32 : 0)
               }px`,
         }}>
         <div ref={headerRef} className={head()}>
