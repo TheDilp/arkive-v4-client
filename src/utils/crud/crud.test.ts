95a8cf46f9d0f4e3352059de1fc816bcd97222e1
Arctur <mihailo.dilparic@gmail.com>
2023-11-14 16:04:15 +0100

    chore: delete tests

--- src/utils/crud/crud.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-
-import { baseURLS } from "../enums";
-import { FetchFunction } from "./FetchFunction";
-
-test("test server connection", async () => {
-  const res = await FetchFunction({ url: `${baseURLS.baseServer}/health_check`, method: "GET" });
-  expect(res).toMatchObject({ basecheck: true, ok: true });
-});
