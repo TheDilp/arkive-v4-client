760975f4eb5fceece8c5115187ba615e124a4c08
Arctur <mihailo.dilparic@gmail.com>
2023-11-17 13:51:19 +0100

    feat: character mention images

--- src/components/Complex/Editor/Extensions/Mention/CharacterMention.tsx
+++ src/components/Complex/Editor/Extensions/Mention/CharacterMention.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 import { useGetEntity } from "../../../../../hooks";
 import { CharacterType } from "../../../../../types";
-import { IconEnum } from "../../../../../utils";
+import { getImageURL, IconEnum } from "../../../../../utils";
+import { Avatar } from "../../../../Misc";
 
 type Props = {
   title?: string;
@@ -16,7 +17,7 @@ export function CharacterMention({ nodeId, project_id, title, nodeLabel }: Props
     nodeId,
     "characters",
     {
-      fields: ["id", "full_name"],
+      fields: ["id", "full_name", "portrait_id"],
     },
     { enabled: !!nodeId, staleTime: 20 * 60 * 1000, queryKeyConcat: ["mention"] },
   );
@@ -26,8 +27,12 @@ export function CharacterMention({ nodeId, project_id, title, nodeLabel }: Props
       className="inline-flex items-center font-lato font-bold underline transition-colors hover:text-sky-400"
       to={!project_id ? `/public/characters/${nodeId}/resources` : `/projects/${project_id}/characters/${nodeId}/resources`}>
       <div className="relative -top-[0.0625rem] flex items-start">
-        <span className="relative">
-          <Icon fontSize={14} icon={IconEnum.character} />
+        <span className="characterMentionImage relative">
+          {data?.data?.portrait_id ? (
+            <Avatar image={getImageURL(project_id as string, "images", data?.data?.portrait_id)} size="xxs" />
+          ) : (
+            <Icon fontSize={14} icon={IconEnum.character} />
+          )}
         </span>
         <span className="text-sm underline">{data?.data?.full_name || title || nodeLabel}</span>
       </div>
