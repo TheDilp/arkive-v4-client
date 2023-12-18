547a4fb40595dde485a425dd99bbc5736bcb758c
Arctur <mihailo.dilparic@gmail.com>
2023-12-18 09:06:30 +0100

    feat: create document when adding to char

--- src/components/Overlay/DrawerContent/CharacterAddDrawer.tsx
+++ src/components/Overlay/DrawerContent/CharacterAddDrawer.tsx
@@ -1,12 +1,15 @@
 import { useResetAtom } from "jotai/utils";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { RemirrorJSON } from "remirror";
 
-import { useAddToEntity } from "../../../hooks";
+import { useAddToEntity, useCreateEntity, useDeleteEntity, useHandleChange } from "../../../hooks";
+import { DocumentType } from "../../../types";
 import { drawerAtom, IconEnum } from "../../../utils";
 import { AddToCharacterSchema, AddToCharacterType } from "../../../validation";
+import { Editor } from "../../Complex";
 import { EntityPreview } from "../../DataDisplay";
-import { Button, Search } from "../../Form";
+import { Button, Input, Search, Title } from "../../Form";
 import { Badge } from "../../Misc";
 
 type Props = {
@@ -19,12 +22,18 @@ type Props = {
 export function CharacterAddDrawer({ data }: Props) {
   const { project_id } = useParams();
   const [items, setItems] = useState<{ label: string; value: string; image?: string; color?: string }[]>([]);
+  const [newDocument, setNewDocument] = useState<Partial<DocumentType>>({ title: "" });
+  const { handleChange } = useHandleChange({ data: newDocument, setData: setNewDocument });
   const resetDrawer = useResetAtom(drawerAtom);
   const { mutateAsync: addToCharacter, isLoading: isMutating } = useAddToEntity<AddToCharacterType>(
     data.id,
     "characters",
     project_id as string,
   );
+  const { mutateAsync: createDocument, isLoading: isCreating } = useCreateEntity<{
+    data: Pick<DocumentType, "id" | "title" | "project_id" | "content">;
+  }>("documents");
+  const { mutate: deleteDocument } = useDeleteEntity("documents", project_id as string, false);
   return (
     <div className="flex flex-col gap-y-2">
       <Search
@@ -33,10 +42,8 @@ export function CharacterAddDrawer({ data }: Props) {
         onChange={async ({ label, value, image, color }) => {
           if (items.some((i) => i.value === value)) {
             setItems((prev) => prev.filter((i) => i.value !== value));
-
             return;
           }
-
           if (label && value) setItems((prev) => (prev || []).concat({ label, image, value, color }));
         }}
         placeholder={`Press enter to search and add ${data?.type}.`}
@@ -64,17 +71,48 @@ export function CharacterAddDrawer({ data }: Props) {
           />
         ))
       )}
+      {data?.type === "documents" ? (
+        <div className="relative flex flex-col gap-y-2">
+          <Title isDrawerTitle label="Or create a new document" />
+          <Input isDisabled={!!items.length} label="Title" name="title" onChange={handleChange} value={newDocument.title} />
+          <Editor initialContent={undefined} isDisabled={!!items.length} name="content" onChange={handleChange} />
+        </div>
+      ) : null}
       <Button
         icon={IconEnum.add}
-        isDisabled={isMutating || items.length === 0}
-        isLoading={isMutating}
+        isDisabled={isMutating || (items.length === 0 && !newDocument.title)}
+        isLoading={isMutating || isCreating}
         label="Save"
         onClick={async () => {
-          const payload = { relations: { [data.type]: items.map((i) => ({ id: i.value })) } };
-          const parsedPayload = AddToCharacterSchema.parse(payload);
-          await addToCharacter(parsedPayload, {
-            onSuccess: resetDrawer,
-          });
+          if (items.length) {
+            const payload = { relations: { [data.type]: items.map((i) => ({ id: i.value })) } };
+            const parsedPayload = AddToCharacterSchema.parse(payload);
+            await addToCharacter(parsedPayload, {
+              onSuccess: resetDrawer,
+            });
+            return;
+          }
+          if (newDocument.title) {
+            const id = crypto.randomUUID();
+            await createDocument({
+              data: {
+                id,
+                title: newDocument.title,
+                content: newDocument.content as RemirrorJSON,
+                project_id: project_id as string,
+              },
+            });
+            await addToCharacter(
+              { relations: { documents: [{ id }] } },
+              {
+                onSuccess: resetDrawer,
+                onError: () => {
+                  deleteDocument({ data: { id } });
+                  resetDrawer();
+                },
+              },
+            );
+          }
         }}
         variant="success"
       />
