2caac41d3584efe00b458bf654bcfec7b9e2ff83
Arctur <mihailo.dilparic@gmail.com>
2023-12-19 11:27:34 +0100

    fix: graph mention

--- src/components/Complex/Editor/Extensions/Mention/GraphMention.tsx
+++ src/components/Complex/Editor/Extensions/Mention/GraphMention.tsx
@@ -19,7 +19,7 @@ function GraphMentionTooltip({ id, project_id }: Pick<Props, "id" | "project_id"
     id as string,
     "graphs",
     { data: { project_id }, fields: ["title"], relations: { nodes: true, edges: true } },
-    { enabled: !!id, queryKeyConcat: ["mention", "tooltip"], retry: false },
+    { enabled: !!id, queryKeyConcat: ["mention", "tooltip"], staleTime: 5 * 60 * 1000, retry: false },
   );
   return (
     <Card title={data?.data?.title || ""}>
@@ -43,7 +43,7 @@ export function GraphMention({ title, id, label, project_id, isPublic }: Props) 
       <Link
         className="mt-0 box-border inline-block h-full items-center border-none font-lato text-sm font-bold underline hover:text-sky-400 focus:outline-none focus-visible:outline-none active:outline-none"
         id={`link-${id}`}
-        to={getMentionLink(project_id as string, "maps", id as string, !!data?.data?.is_public, isPublic)}>
+        to={getMentionLink(project_id as string, "graphs", id as string, !!data?.data?.is_public, isPublic)}>
         <div className="top-[0.025rem] flex items-start">
           <span className="relative top-0.5">
             <Icon fontSize={15} icon={IconEnum.graph} />
