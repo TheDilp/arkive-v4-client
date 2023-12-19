e64ad52f1b7b977c1bc81ab6e8526f16fba2c1a3
Arctur <mihailo.dilparic@gmail.com>
2023-12-19 08:39:42 +0100

    chore: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
