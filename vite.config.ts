5204033692ba8cb724278aa6d36cc0973ca8cb36
Arctur <mihailo.dilparic@gmail.com>
2023-12-25 12:21:46 +0100

    chore: remove chunking

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (id.includes("react")) return "react";
           if (id.includes("3d-dice")) return "dice";
-          if (id.includes("cytoscape")) return "cytoscape";
-          if (id.includes("clerk")) return "clerk";
-          if (id.includes("@tanstack")) return "tanstack";
-          if (id.includes("remirror")) return "remirror";
           return "vendor";
         },
       },
