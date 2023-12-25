1c542307ae9ba028da194b3706372bbcfed03fc8
Arctur <mihailo.dilparic@gmail.com>
2023-12-25 09:54:38 +0100

    feat: gzip compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
+    viteCompression(),
     react({
       babel: {
         parserOpts: {
