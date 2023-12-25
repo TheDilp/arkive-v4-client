cc3bea7304a805287760708c6e3bb4ac0258ff4e
Arctur <mihailo.dilparic@gmail.com>
2023-12-25 12:10:20 +0100

    chore: remove package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteCompression from "vite-plugin-compression";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    viteCompression(),
     react({
       babel: {
         parserOpts: {
@@ -17,11 +15,5 @@ export default defineConfig({
   ],
   build: {
     minify: true,
-  },
-  test: {
-    includeSource: ["src/**/*.{js,ts}"],
-    coverage: {
-      reporter: ["text"],
-    },
   },
 });
