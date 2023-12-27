8ebb5e7062737a71aaedb664e12d4ca0298f794a
Arctur <mihailo.dilparic@gmail.com>
2023-12-27 10:19:16 +0100

    feat: map preview for map pins

--- src/components/Complex/Editor/Extensions/Mention/MapPinMention.tsx
+++ src/components/Complex/Editor/Extensions/Mention/MapPinMention.tsx
@@ -3,10 +3,13 @@
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
-import { useGetSubEntity } from "../../../../../hooks";
-import { MapPinType } from "../../../../../types";
+import { useGetEntity, useGetSubEntity } from "../../../../../hooks";
+import { MapView } from "../../../../../pages/Entities";
+import { MapPinType, MapType } from "../../../../../types";
 import { getImageURL, getMentionLink, IconEnum } from "../../../../../utils";
+import { Card } from "../../../../Layout";
 import { Avatar } from "../../../../Misc";
+import { Tooltip } from "../../../../Overlay";
 
 type Props = {
   title?: string;
@@ -16,6 +19,23 @@ type Props = {
   parent_id: string | undefined;
   isPublic?: boolean;
 };
+
+function MapPinMentionTooltip({ id, parent_id, project_id }: Pick<Props, "id" | "parent_id" | "project_id">) {
+  const { data } = useGetEntity<MapType>(
+    parent_id as string,
+    "maps",
+    { data: { project_id }, fields: ["title", "image_id"], relations: { map_pins: true } },
+    { enabled: !!parent_id, queryKeyConcat: ["mention", "tooltip"], staleTime: 5 * 60 * 1000, retry: false },
+  );
+  return (
+    <Card title={data?.data?.title || ""}>
+      <div className="h-96 min-h-[24rem] w-96 min-w-[24rem] overflow-y-auto whitespace-pre-line">
+        {data?.data ? <MapView center_on={id} data={data?.data} isReadOnly isViewOnly /> : null}
+      </div>
+    </Card>
+  );
+}
+
 export function MapPinMention({ title, id, label, project_id, isPublic, parent_id }: Props) {
   const { data } = useGetSubEntity<MapPinType>(
     id as string,
@@ -25,22 +45,30 @@ export function MapPinMention({ title, id, label, project_id, isPublic, parent_i
     },
     { enabled: !!id, staleTime: 5 * 60 * 1000, queryKeyConcat: ["mention"], retry: false, isPublic },
   );
-
   return id ? (
-    <Link
-      className="inline-flex items-center font-lato text-sm font-bold transition-colors"
-      to={getMentionLink(id as string, "map_pins", project_id as string, data?.data?.is_public ?? false, isPublic, parent_id)}>
-      <div className="flex items-start">
-        {data?.data?.image_id ? (
-          <span className="characterMentionImage" onClick={(e) => e.preventDefault()}>
-            <Avatar hasShowImage image={getImageURL(project_id as string, "images", data?.data?.image_id)} size="3xs" />
-          </span>
-        ) : (
-          <Icon fontSize={15} icon={IconEnum.map_pin} />
-        )}
-        <span className="underline hover:text-sky-400">{data?.data?.title || title || label || ""}</span>
-      </div>
-    </Link>
+    <Tooltip content={<MapPinMentionTooltip id={id} parent_id={parent_id} project_id={project_id} />}>
+      <Link
+        className="inline-flex items-center font-lato text-sm font-bold transition-colors"
+        to={getMentionLink(
+          id as string,
+          "map_pins",
+          project_id as string,
+          data?.data?.is_public ?? false,
+          isPublic,
+          parent_id,
+        )}>
+        <div className="flex items-start">
+          {data?.data?.image_id ? (
+            <span className="characterMentionImage" onClick={(e) => e.preventDefault()}>
+              <Avatar hasShowImage image={getImageURL(project_id as string, "images", data?.data?.image_id)} size="3xs" />
+            </span>
+          ) : (
+            <Icon fontSize={15} icon={IconEnum.map_pin} />
+          )}
+          <span className="underline hover:text-sky-400">{data?.data?.title || title || label || ""}</span>
+        </div>
+      </Link>
+    </Tooltip>
   ) : (
     <span className="font-lato text-sm">{label || ""}</span>
   );
