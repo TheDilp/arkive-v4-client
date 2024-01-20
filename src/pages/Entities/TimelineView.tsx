6c79b060a7a2bbc6ea7bf091b6ffc70571b6f9c5
Arctur <mihailo.dilparic@gmail.com>
2024-01-20 14:17:01 +0100

    feat: tooltip for point events

--- src/pages/Entities/TimelineView.tsx
+++ src/pages/Entities/TimelineView.tsx
@@ -2,6 +2,7 @@ import * as d3 from "d3";
 import groupBy from "lodash.groupby";
 import { MouseEvent, MutableRefObject, useLayoutEffect, useRef } from "react";
 
+import { useBreakpoint } from "../../hooks";
 import { EventType, MonthType } from "../../types";
 import { DefaultTagColor } from "../../utils";
 
@@ -11,6 +12,9 @@ const XAXISOFFSET = 20;
 export function TimelineView({ events, months }: { events: EventType[]; months: MonthType[] }) {
   const timelineContainer = useRef() as MutableRefObject<SVGSVGElement>;
   const container = useRef() as MutableRefObject<HTMLDivElement>;
+
+  const { isLg } = useBreakpoint();
+
   // const firstRender = useRef(true);
   useLayoutEffect(() => {
     if (
@@ -52,6 +56,8 @@ export function TimelineView({ events, months }: { events: EventType[]; months: 
           x: (e.start_year - 1) * monthCount + e.start_month + e.start_day * 0.01,
           y: e.start_year + 0.5,
           background_color: e.background_color || DefaultTagColor,
+          title: e.title,
+          description: e.description,
         };
       });
       svg
@@ -60,7 +66,24 @@ export function TimelineView({ events, months }: { events: EventType[]; months: 
         .attr("transform", `translate(0,${height / 1.05})`)
         .call(axisBottom);
 
+      const tooltip = d3
+        .select(container.current)
+        .attr("class", "tooltip absolute hidden bg-black border border-zinc-800 shadow-lg font-lato rounded p-4");
+
+      const highlighter = svg
+        .append("rect")
+        .attr("class", "highlighter stroke-blue-400 w-[1px]")
+        .attr("height", height)
+        .attr("width", width)
+        .style("pointer-events", "all")
+        .style("fill", "none")
+        .on("mouseout", () => {});
+      svg.on("mousemove", (e: MouseEvent) => {
+        highlighter.attr("transform", `translate(${e.clientX + container.current.scrollLeft - (isLg ? padding - 20 : 16)}, 0)`);
+      });
+
       const groupForCircles = svg.append("g").attr("id", "groupForCircles");
+
       points.forEach((e) => {
         groupForCircles
           .append("g")
@@ -68,22 +91,21 @@ export function TimelineView({ events, months }: { events: EventType[]; months: 
           .attr("cx", x(e.x))
           .attr("cy", y(e.y))
           .attr("r", circleRadius)
-          .style("fill", e.background_color);
-      });
-
-      const highlighter = svg
-        .append("rect")
-        .attr("class", "highlighter")
-        .attr("height", height)
-        .attr("width", width)
-        .style("pointer-events", "all")
-        .style("fill", "none")
-        .on("mouseover", (e: MouseEvent) => {
-          e.currentTarget.classList.add("stroke-blue-400", "w-[1px]", "absolute");
-        })
-        .on("mouseout", () => {});
-      svg.on("mousemove", (e: MouseEvent) => {
-        highlighter.attr("transform", `translate(${e.clientX + container.current.scrollLeft - padding + 20}, 0)`);
+          .style("fill", e.background_color)
+          .on("mouseover", (evt: MouseEvent) => {
+            tooltip
+              .style("display", "block")
+              .style(
+                "transform",
+                `translate(${Number(evt.currentTarget.getAttribute("cx")) + 5 ?? 0}px, ${
+                  Number(evt.currentTarget.getAttribute("cy")) ?? 0
+                }px)`,
+              )
+              .html(e.title);
+          })
+          .on("mouseout", () => {
+            tooltip.style("display", "none");
+          });
       });
     }
 
@@ -91,11 +113,13 @@ export function TimelineView({ events, months }: { events: EventType[]; months: 
       d3.select(timelineContainer.current).select("#groupForCircles").remove();
       d3.select(timelineContainer.current).select(".axis--x").remove();
       d3.select(timelineContainer.current).select(".highlighter").remove();
+      d3.select(timelineContainer.current).select(".tooltip").remove();
     };
   }, [events]);
 
   return (
-    <div ref={container} className="h-full w-full max-w-full overflow-x-auto overflow-y-hidden">
+    <div className="relative h-full w-full max-w-full overflow-x-auto overflow-y-hidden">
+      <div ref={container} className="hidden" />
       <div className="h-full w-[200%]">
         {events.length ? <svg ref={timelineContainer} className="block h-full min-w-full bg-zinc-900" /> : null}
       </div>
