a0db5171655da54ca190ca01560b86e121fd2e7f
Arctur <mihailo.dilparic@gmail.com>
2024-01-23 15:57:54 +0100

    chore: adjust thumbnail size for avatar

--- src/utils/ui/avatarUtils.ts
+++ src/utils/ui/avatarUtils.ts
@@ -4,6 +4,6 @@ export function getAvatarThumbnailDimensions(size: Size): { width: number; heigh
   if (size === "3xs") return { width: 12, height: 12 };
   if (size === "sm") return { width: 35, height: 35 };
   if (size === "md") return { width: 40, height: 40 };
-  if (size === "4xl") return { width: 96, height: 96 };
+  if (size === "4xl") return { width: 200, height: 200 };
   return { width: 50, height: 50 };
 }
