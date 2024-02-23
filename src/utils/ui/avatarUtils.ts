eb81b260a37cf69ba608072cadb323636313211f
Arctur <mihailo.dilparic@gmail.com>
2024-02-23 12:09:04 +0100

    feat: avatar thumbnail size

--- src/utils/ui/avatarUtils.ts
+++ src/utils/ui/avatarUtils.ts
@@ -4,6 +4,7 @@ export function getAvatarThumbnailDimensions(size: Size): { width: number; heigh
   if (size === "3xs") return { width: 12, height: 12 };
   if (size === "sm") return { width: 35, height: 35 };
   if (size === "md") return { width: 40, height: 40 };
+  if (size === "2xl") return { width: 160, height: 160 };
   if (size === "3xl") return { width: 180, height: 180 };
   if (size === "4xl") return { width: 200, height: 200 };
   return { width: 50, height: 50 };
