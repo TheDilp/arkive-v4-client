b832bab33870e4780215eabed3c06cf9057f6282
Arctur <mihailo.dilparic@gmail.com>
2024-03-08 13:23:12 +0100

    feat: invalidate queries if roles update

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -133,6 +133,8 @@ export function Navbar({ isDisabled }: { isDisabled: boolean }) {
           });
           queryClient.invalidateQueries(["allEntities", project_id, entityType]);
         }
+      } else if (lastJsonMessage.event_type === "ROLE_UPDATED") {
+        queryClient.invalidateQueries(["allEntities", project_id]);
       }
     }
   }, [lastJsonMessage]);
