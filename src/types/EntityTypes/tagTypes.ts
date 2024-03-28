0133dc827f2981a1084ccadc7f1fdcec55c7db18
Arctur <mihailo.dilparic@gmail.com>
2024-03-28 13:51:03 +0100

    chore: update tag type

--- src/types/EntityTypes/tagTypes.ts
+++ src/types/EntityTypes/tagTypes.ts
@@ -1,6 +1,10 @@
+import { EntityPermissionType } from "./baseEntityTypes";
+
 export interface TagType {
   id: string;
   title: string;
   color: string;
   project_id: string;
+  owner_id: string;
+  permissions: EntityPermissionType[];
 }
