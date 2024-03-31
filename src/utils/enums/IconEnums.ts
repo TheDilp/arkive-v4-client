4618f4b74efdf7812f6dd0f6c56ecc45128440ac
Arctur <mihailo.dilparic@gmail.com>
2024-03-31 11:42:30 +0200

    feat: add restore icon

--- src/utils/enums/IconEnums.ts
+++ src/utils/enums/IconEnums.ts
@@ -93,6 +93,7 @@ export const IconEnum = {
   range: "ph:rows" as const,
   reload: "ph:arrow-clockwise" as const,
   remove: "ph:minus" as const,
+  restore: "ph:box-arrow-up" as const,
   save: "ph:floppy-disk" as const,
   screen: "fluent:board-24-regular" as const,
   search: "ph:magnifying-glass-light" as const,
