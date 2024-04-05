6744826e433c9c10123b7ac1a29ece92b941fab3
Mihailo <mihailo.dilparic@gmail.com>
2024-04-05 18:15:37 +0200

    chore: change production

--- vite.config.ts
+++ vite.config.ts
@@ -26,11 +26,7 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (id.includes("3d-dice") || id.includes("world.offscreen") || id.includes("world.onscreen")) return "dice";
-          if (id.includes("remirror")) return "remirror";
-          if (id.includes("leaflet")) return "leaflet";
-          if (id.includes("cytoscape")) return "cytoscape";
-          if (id.includes("lodash")) return "lodash";
-          if (id.includes("prosemirror")) return "prosemirror";
+
           return "vendor";
         },
       },
