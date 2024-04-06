26fc1ddec398842f55591923e2f3b601dbd2642f
Mihailo <mihailo.dilparic@gmail.com>
2024-04-06 14:23:08 +0200

    feat: owner id and permissions for words

--- src/types/EntityTypes/wordTypes.ts
+++ src/types/EntityTypes/wordTypes.ts
@@ -1,9 +1,13 @@
+import { EntityPermissionType } from "./baseEntityTypes";
+
 export interface WordType {
   id: string;
+  owner_id: string;
   title: string;
   translation: string;
   parent_id: string;
   description?: string;
+  permissions: EntityPermissionType[];
 }
 
 export type WordStateType = Partial<WordType>;
