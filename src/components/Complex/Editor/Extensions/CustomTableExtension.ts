8e5358c5d71d36934a842b19ba4c9729b7c98f2c
Mihailo <mihailo.dilparic@gmail.com>
2024-04-08 10:27:50 +0200

    chore: change table shortcuts

--- src/components/Complex/Editor/Extensions/CustomTableExtension.ts
+++ src/components/Complex/Editor/Extensions/CustomTableExtension.ts
@@ -2,7 +2,7 @@ import { keyBinding, KeyBindingProps } from "remirror";
 import { TableExtension } from "remirror/extensions";
 
 export class CustomTableExtension extends TableExtension {
-  @keyBinding({ shortcut: "Mod-Shift-Delete", command: "deleteTable" })
+  @keyBinding({ shortcut: "Shift-Mod-Delete", command: "deleteTable" })
   delTableShortcut(props: KeyBindingProps): boolean {
     return this.deleteTable()(props);
   }
@@ -17,12 +17,12 @@ export class CustomTableExtension extends TableExtension {
     return this.deleteTableRow()(props);
   }
 
-  @keyBinding({ shortcut: "Mod-Insert", command: "addCol" })
+  @keyBinding({ shortcut: "Shift-Mod-Insert", command: "addCol" })
   addColShortcut(props: KeyBindingProps): boolean {
     return this.addTableColumnAfter()(props);
   }
 
-  @keyBinding({ shortcut: "Insert", command: "addRow" })
+  @keyBinding({ shortcut: "Mod-Insert", command: "addRow" })
   addRowShortcut(props: KeyBindingProps): boolean {
     this.setTableCellAttribute("selected", true);
     return this.addTableRowAfter()(props);
