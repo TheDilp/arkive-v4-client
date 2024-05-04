48269841e721197c96aa56b1874b9e4c22eb6638
Mihailo <mihailo.dilparic@gmail.com>
2024-05-04 16:15:38 +0200

    chore: add clearable to tag view quick search

--- src/pages/Entities/TagView.tsx
+++ src/pages/Entities/TagView.tsx
@@ -359,6 +359,7 @@ export function TagView() {
       <div className="sticky top-0 flex h-12 w-full items-center justify-end gap-x-2">
         <div className="w-52">
           <Input
+            isClearable
             name="quick_filter"
             onChange={({ value }) => setFilter(value as string)}
             placeholder="Quick search by title"
