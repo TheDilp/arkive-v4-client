6ef7177b1d4a9ad1107d54743172cb6110428dab
Mihailo <mihailo.dilparic@gmail.com>
2024-05-04 13:49:04 +0200

    chore: add types

--- src/types/ComponentTypes/ComplexTypes/ImageSelectTypes.ts
+++ src/types/ComponentTypes/ComplexTypes/ImageSelectTypes.ts
@@ -1,9 +1,10 @@
-import { AssetType } from "../../baseTypes";
+import { AssetType, Variant } from "../../baseTypes";
 import { ButtonType, SearchType } from "..";
 
 export interface ImageSelectType extends Pick<SearchType, "value" | "name" | "helperText">, Pick<ButtonType, "isIconOnly"> {
   label?: string;
   type: AssetType;
   isDisabled?: boolean;
+  variant?: Variant;
   onChange: ({ name, value, label }: { name: string; value: string | null; label?: string }) => void;
 }
