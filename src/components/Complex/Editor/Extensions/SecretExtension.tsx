a87f322bd6d085c8ee92159c120b677a744edb31
Mihailo <mihailo.dilparic@gmail.com>
2024-05-25 18:49:26 +0200

    fix: secret extension new line

--- src/components/Complex/Editor/Extensions/SecretExtension.tsx
+++ src/components/Complex/Editor/Extensions/SecretExtension.tsx
@@ -66,7 +66,7 @@ class SecretExtension extends NodeExtension<SecretOptions> {
     const { classNames, secret } = this.options;
 
     return {
-      content: "block",
+      content: "block+",
       defining: true,
       draggable: false,
       ...override,
