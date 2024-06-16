1d5469fbf6daa9a235a9d0a9a41ed940cb6ea408
Mihailo <mihailo.dilparic@gmail.com>
2024-06-16 15:23:43 +0200

    feat: initial dyce vtt

--- arkive-v4-dyce-vtt/vite.config.ts
+++ arkive-v4-dyce-vtt/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    fs: {
+      allow: [
+        "./",
+        "../components",
+        "../hooks",
+        "../utils",
+        "..validation",
+        "../public/assets",
+        "../public/Lato-Regular.ttf",
+        "../public/Merriweather-Regular.ttf",
+        "../public/Lato-Bold.ttf",
+        "../public/Merriweather-Bold.ttf",
+      ],
+    },
+  },
+});
+
