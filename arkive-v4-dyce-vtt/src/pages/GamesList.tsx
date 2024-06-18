3aa7d1f27d5667661869d15cce1ddc9fced65ef6
Mihailo <mihailo.dilparic@gmail.com>
2024-06-18 13:13:42 +0200

    feat: create game button

--- arkive-v4-dyce-vtt/src/pages/GamesList.tsx
+++ arkive-v4-dyce-vtt/src/pages/GamesList.tsx
@@ -1,18 +1,19 @@
 import { useAtomValue } from "jotai";
 
-import { ProjectGameCard } from "../../../components";
+import { Button, ProjectGameCard } from "../../../components";
 import { useGetEntities } from "../../../hooks";
 import { GameType } from "../../../types";
-import { userAtom } from "../../../utils";
+import { IconEnum, userAtom } from "../../../utils";
 
 export function GamesList() {
   const user = useAtomValue(userAtom);
-  const { data } = useGetEntities<GameType>({ fields: [], data: { user_id: "00fb25f1-2f47-40e3-bcaf-35d303c10207" } }, "games");
-
-  console.log(user);
+  const { data } = useGetEntities<GameType>({ fields: [], data: { user_id: user?.auth_id } }, "games");
 
   return (
     <div className="p-4">
+      <div className="ml-auto w-min">
+        <Button icon={IconEnum.add} label="Create new game" onClick={undefined} />
+      </div>
       <div className="grid h-full max-h-full flex-1 grid-cols-1 gap-4 overflow-auto xl:grid-cols-2 2xl:grid-cols-4">
         {(data?.data || [])?.map((game) => (
           <ProjectGameCard feature_flags={{}} id={game.id} key={game.id} module="dyce_vtt" title={game.title}></ProjectGameCard>
