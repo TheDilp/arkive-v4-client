726557d97c8b255494c9a3a8429fdd3921860af0
Mihailo <mihailo.dilparic@gmail.com>
2024-06-23 10:04:15 +0200

    feat: user status type

--- types/userTypes.ts
+++ types/userTypes.ts
@@ -13,4 +13,12 @@ export interface UserType {
   permissions: PermissionType[];
 }
 
+export type UserStatusType = {
+  status: "authenticated" | "unauthenticated";
+  user_id: string;
+  project_id: string | null;
+  name: string | null;
+  image_url: string | null;
+};
+
 export type UserHasPermissionsType = Partial<Record<PermissionCodeType | "is_owner", boolean>>;
