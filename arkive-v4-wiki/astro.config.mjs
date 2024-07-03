eb640059ddf3d354d8733fa3a7a9aad240fe5873
Mihailo Dilparic <mihailo@salestrekker.com>
2024-07-03 13:29:54 +0200

    feat: icons and collapsible component

--- arkive-v4-wiki/astro.config.mjs
+++ arkive-v4-wiki/astro.config.mjs
@@ -1,8 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()]
 });
