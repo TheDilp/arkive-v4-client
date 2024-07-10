d22894208145b2e92411ae5002b896c0b0c6ac26
Mihailo <mihailo.dilparic@gmail.com>
2024-07-10 09:20:29 +0200

    chore: root tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{ts,tsx}",
+    "./hooks /**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./index.css",
+    "./Editor.css",
+    "./utils/**/*.tsx",
+  ],
+
+  theme: {
+    extend: {
+      transitionProperty: {
+        width: "width",
+      },
+      animation: {
+        progress: "progress 1s infinite linear",
+      },
+      keyframes: {
+        progress: {
+          "0%": { transform: " translateX(0) scaleX(0)" },
+          "40%": { transform: "translateX(0) scaleX(0.4)" },
+          "100%": { transform: "translateX(100%) scaleX(0.5)" },
+        },
+      },
+      transformOrigin: {
+        "left-right": "0% 50%",
+      },
+      fontFamily: {
+        lato: ["Lato"],
+        merriweather: ["Merriweather"],
+      },
+      screens: {
+        xxs: "320px",
+        xs: "480px",
+      },
+    },
+  },
+};
+
