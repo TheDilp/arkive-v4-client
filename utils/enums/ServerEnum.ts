03cfedb6fc5c06f8752b7c2b9ccb46bf898bf0d6
Mihailo <mihailo.dilparic@gmail.com>
2024-07-20 12:22:01 +0200

    fix: url

--- utils/enums/ServerEnum.ts
+++ utils/enums/ServerEnum.ts
@@ -2,9 +2,7 @@ export const baseURLS = {
   baseServer: `${
     import.meta.env.DEV ? `http://localhost:${import.meta.env.VITE_SERVER_PORT}` : import.meta.env.VITE_ARKIVE_EDITOR_SERVER
   }/api/v1`,
-  basePublicServer: `${
-    import.meta.env.DEV ? import.meta.env.VITE_PUBLIC_SERVER : import.meta.env.VITE_ARKIVE_EDITOR_SERVER
-  }/public`,
+  basePublicServer: `${import.meta.env.DEV ? "http://localhost:5178" : import.meta.env.VITE_ARKIVE_WIKI_SERVER}`,
 
   baseThumbnailServer: "https://arkive-v4-thumbnail-service.up.railway.app",
   baseWebsocketServer: `${
