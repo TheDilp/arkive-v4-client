a75036523cb09c5899ec483c90d75bd0f3c0c27d
Mihailo <mihailo.dilparic@gmail.com>
2024-07-27 09:50:38 +0200

    feat: basic routing for gateway

--- arkive-v4-gateway/src/App.tsx
+++ arkive-v4-gateway/src/App.tsx
@@ -1,5 +1,27 @@
+import { Navigate, Route, Routes, To } from "react-router-dom";
+import CharacterForm from "./pages/CharacterForm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <main className="bg-blue-300">test</main>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main className="h-screen max-h-screen w-screen overflow-hidden">
+        <Routes>
+          <Route path="characters/:access_id/:entity_id" element={<CharacterForm />} />
+          <Route path="*" element={<Navigate to={-1 as To} />} />
+        </Routes>
+      </main>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
