58a34cfc2e36e29d02e9f3d2d910a7563eda5443
Mihailo <mihailo.dilparic@gmail.com>
2024-08-14 12:49:17 +0200

    chore: remove package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { compression } from "vite-plugin-compression2";
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: any }) =>
@@ -14,7 +13,6 @@ export default ({ mode }: { mode: any }) =>
     },
 
     plugins: [
-      nodePolyfills(),
       react({
         babel: {
           parserOpts: {
