5fab26c2007b8c9d243534658e5764ba60db74f0
Mihailo <mihailo.dilparic@gmail.com>
2024-08-15 13:15:07 +0200

    feat: gateway specific error page

--- pages/Misc/ErrorPage.tsx
+++ pages/Misc/ErrorPage.tsx
@@ -22,3 +22,20 @@ export function ErrorPage() {
     </div>
   );
 }
+
+export function GatewayErrorPage() {
+  return (
+    <div className="grid h-screen place-content-center bg-zinc-950 px-4">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-red-600">Error</h1>
+        <p className="text-2xl font-bold tracking-tight text-zinc-400 sm:text-4xl">Uh-oh!</p>
+        <p className="mt-4 text-zinc-500">There was an error.</p>
+        <Link
+          className="mt-6 inline-block rounded bg-blue-600 px-5 py-3 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring"
+          to="../name">
+          Go to code access
+        </Link>
+      </div>
+    </div>
+  );
+}
