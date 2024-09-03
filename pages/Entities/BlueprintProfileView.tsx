6a1416253d80ca34f194b787f8b6822879fcaaab
Mihailo <mihailo.dilparic@gmail.com>
2024-09-03 10:59:31 +0200

    chore: change bg color

--- pages/Entities/BlueprintProfileView.tsx
+++ pages/Entities/BlueprintProfileView.tsx
@@ -229,11 +229,11 @@ export function BlueprintProfileView({ id, parent_id, isViewOnly }: { id?: strin
       <div className="w-full flex-1 flex-col content-start gap-4 pt-0">
         {isLoading ? <Skeleton type="character_profile" /> : null}
 
-        <div className="flex max-h-full flex-1 flex-col overflow-auto rounded-lg bg-zinc-900 p-4 lg:col-span-4">
+        <div className="flex max-h-full flex-1 flex-col overflow-auto rounded-lg bg-zinc-950 p-4 lg:col-span-4">
           <div className="flex flex-col gap-y-2">
             {isEditable ? (
               <Collapsible icon={IconEnum.info_circle} label="Basic info">
-                <div className="flex w-full items-center gap-x-6 bg-zinc-800 p-2">
+                <div className="flex w-full items-center gap-x-6 p-2">
                   <div className="flex-1">
                     <Input
                       isDisabled={!permissions?.update_blueprint_instances}
@@ -259,7 +259,7 @@ export function BlueprintProfileView({ id, parent_id, isViewOnly }: { id?: strin
 
             <Collapsible icon={IconEnum.additional_fields} initialOpen label="Fields">
               {isEditable ? (
-                <div className="bg-zinc-800 p-2">
+                <div className="p-2">
                   <RelatedEntityForm
                     fields={blueprint?.data?.blueprint_fields || []}
                     fields_data={blueprintInstance?.blueprint_fields || []}
@@ -310,7 +310,7 @@ export function BlueprintProfileView({ id, parent_id, isViewOnly }: { id?: strin
                 icon={IconEnum.tags}
                 initialOpen={false}
                 label="Tags">
-                <div className={`flex flex-col p-2 ${isEditable ? "bg-zinc-800" : ""}`}>
+                <div className={`flex flex-col p-2 ${isEditable ? "" : ""}`}>
                   {blueprintInstance?.tags?.length && !isEditable ? (
                     <div className="animate-in fade-in fill-mode-both flex w-full flex-wrap gap-2">
                       {blueprintInstance.tags.map((tag) => (
