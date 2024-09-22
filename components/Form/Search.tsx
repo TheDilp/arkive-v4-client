5eb029b832ce0a7f0fb13cbcfbe8aac29c46b5dd
Mihailo <mihailo.dilparic@gmail.com>
2024-09-22 13:07:51 +0200

    chore: pass parent_id

--- components/Form/Search.tsx
+++ components/Form/Search.tsx
@@ -345,7 +345,7 @@ export function Search({
                   ...prev,
                   title: "Browse",
                   type: "browse_entities",
-                  data: { isMultiple, type: searchEntity, onChange, imageType },
+                  data: { isMultiple, type: searchEntity, onChange, imageType, parent_id },
                   size: "full",
                   isOverlay: true,
                 }))
