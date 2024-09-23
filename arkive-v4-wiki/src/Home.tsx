3a1a74c292b1c46cca4452599991a304a6542862
Mihailo <mihailo.dilparic@gmail.com>
2024-09-23 17:51:52 +0200

    chore: initial get infinite projects

--- arkive-v4-wiki/src/Home.tsx
+++ arkive-v4-wiki/src/Home.tsx
@@ -1,23 +1,44 @@
 import { WikiProjectCard } from "../../components";
-import { useGetEntities } from "../../hooks";
+import { useGetInfiniteEntities } from "../../hooks";
 import { ProjectType } from "../../types";
 
 export function Home() {
-  const { data } = useGetEntities<ProjectType>({ fields: [] }, "projects");
+  const { data: cardData } = useGetInfiniteEntities<ProjectType>(
+    {
+      data: {
+        project_id: "all",
+      },
+      fields: ["id", "title", "description", "image_id"],
+      pagination: {
+        limit: 12,
+      },
+    },
+    "projects",
+    {
+      enabled: true,
+      keepPreviousData: true,
+      getNextPageParam: (_, allPages) => {
+        if (allPages[allPages.length - 1]?.data?.length < 10) return undefined;
+        return allPages.length;
+      },
+    }
+  );
 
   return (
     <div className="flex h-screen max-h-screen w-screen flex-col p-4">
-      <div className="grid grid-cols-4 gap-4">
-        {(data?.data || []).map((project) => (
-          <WikiProjectCard
-            key={project.id}
-            description={project.description}
-            id={project.id}
-            image_id={project.image_id}
-            owner={project.owner}
-            title={project.title}
-          />
-        ))}
+      <div className="grid max-h-full grid-cols-4 gap-4 overflow-y-auto">
+        {(cardData?.pages || [])?.map((page) =>
+          page.data.map((project) => (
+            <WikiProjectCard
+              key={project.id}
+              description={project.description}
+              id={project.id}
+              image_id={project.image_id}
+              owner={project.owner}
+              title={project.title}
+            />
+          ))
+        )}
       </div>
     </div>
   );
--- hooks/crud/readHooks.ts
+++ hooks/crud/readHooks.ts
@@ -286,7 +286,7 @@ export function useGetInfiniteEntities<ReturnType>(
     return FetchFunction({
       method: "POST",
       body: JSON.stringify(finalRequest),
-      url: `${baseURLS.baseServer}/${type.toLowerCase()}`,
+      url: `${getServerUrl()}/${type.toLowerCase()}`,
     });
   }
   const configuredOptions = {
