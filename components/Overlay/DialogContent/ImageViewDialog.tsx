0bf1e8e1b222e5637c8ccb8df99f73745cca98ae
Mihailo <mihailo.dilparic@gmail.com>
2024-10-06 15:04:20 +0200

    fix: image preview dialog showing broken image before load

--- components/Overlay/DialogContent/ImageViewDialog.tsx
+++ components/Overlay/DialogContent/ImageViewDialog.tsx
@@ -13,7 +13,7 @@ export function ImageViewDialog({
   const image_url = useImageURL(
     data?.image_id ? getAssetURL((data?.manual_project_id || project_id) as string, data?.image_type, data?.image_id) : null
   );
-
+  if (!data?.image_id || (!data?.url && !image_url)) return null;
   return (
     <div className="flex h-full w-full select-none flex-col items-center justify-center gap-y-2">
       <img
