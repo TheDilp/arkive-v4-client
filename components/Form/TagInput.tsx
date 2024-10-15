cfb963b2d26e37ca189b57b33e502591e505a8fd
Mihailo <mihailo.dilparic@gmail.com>
2024-10-15 12:42:59 +0200

    feat: select for tag input

--- components/Form/TagInput.tsx
+++ components/Form/TagInput.tsx
@@ -1,8 +1,12 @@
+import { useAtomValue } from "jotai";
+import { useMemo } from "react";
 import { useParams } from "react-router-dom";
 
 import { TagType, Variant } from "../../types";
+import { availableTagsAtom } from "../../utils";
 import { Badge } from "../Misc";
 import { Search } from "./Search";
+import { Select } from "./Select";
 
 type Props = {
   label?: string;
@@ -24,38 +28,73 @@ export function TagInput({
   variant,
 }: Props) {
   const { project_id } = useParams();
+  const allAvailableTags = useAtomValue(availableTagsAtom);
+
+  const formattedAvailableTags = useMemo(
+    () => allAvailableTags.map((tag) => ({ value: tag.id, label: tag.title, color: tag.color })),
+    [allAvailableTags]
+  );
+
   return (
     <div className="flex flex-col gap-y-2">
-      <Search
-        isAutofocused={isAutofocused ?? true}
-        isDisabled={isDisabled}
-        isMultiple={isMultiple}
-        label={componentLabel || ""}
-        name="tags"
-        onChange={({ name, label, value, color }) => {
-          if ((tags || [])?.some((tag) => tag.id === value)) {
+      {formattedAvailableTags.length ? (
+        <Select
+          hasSearch
+          isDisabled={isDisabled}
+          isMultiple={isMultiple}
+          label={componentLabel || ""}
+          name="tags"
+          onChange={({ value }) => {
+            if (Array.isArray(value)) {
+              const tags = allAvailableTags.filter((t) => value.includes(t.id));
+              handleChange({ name: "tags", value: tags });
+            } else if (!isMultiple && !Array.isArray(value)) {
+              if ((allAvailableTags || [])?.some((tag) => tag.id === value)) {
+                handleChange({
+                  name: "tags",
+                  value: [],
+                });
+              } else {
+                const tag = allAvailableTags.find((t) => t.id === value);
+                if (tag) handleChange({ name: "tags", value: [tag] });
+              }
+            }
+          }}
+          options={formattedAvailableTags}
+          value={tags.map((t) => t.id)}
+        />
+      ) : (
+        <Search
+          isAutofocused={isAutofocused ?? true}
+          isDisabled={isDisabled}
+          isMultiple={isMultiple}
+          label={componentLabel || ""}
+          name="tags"
+          onChange={({ name, label, value, color }) => {
+            if ((tags || [])?.some((tag) => tag.id === value)) {
+              handleChange({
+                name,
+                value: (tags || []).filter((t) => t.id !== value),
+              });
+              return;
+            }
+
             handleChange({
               name,
-              value: (tags || []).filter((t) => t.id !== value),
+              value: (tags || []).concat({
+                title: label as string,
+                id: value,
+                project_id: project_id as string,
+                color: color as string,
+              }),
             });
-            return;
-          }
-
-          handleChange({
-            name,
-            value: (tags || []).concat({
-              title: label as string,
-              id: value,
-              project_id: project_id as string,
-              color: color as string,
-            }),
-          });
-        }}
-        placeholder="Type at least 2 characters"
-        searchEntity="tags"
-        value={isMultiple ? tags.map((t) => t.id) : undefined}
-        variant={variant || "primary"}
-      />
+          }}
+          placeholder="Type at least 2 characters"
+          searchEntity="tags"
+          value={isMultiple ? tags.map((t) => t.id) : undefined}
+          variant={variant || "primary"}
+        />
+      )}
 
       <div className="flex flex-wrap gap-2">
         {tags?.length
