5d3d7dfd2d02ae5c9eeff194c13f837c28d32da6
Mihailo <mihailo.dilparic@gmail.com>
2024-12-23 13:14:21 +0100

    chore: add log

--- utils/ui/diceRollerUtils.tsx
+++ utils/ui/diceRollerUtils.tsx
@@ -19,7 +19,8 @@ const externalThemes = diceThemeNames.reduce(
   },
   {} as Record<string, string>
 );
-
+// eslint-disable-next-line no-console
+console.info("IS_PUBLIC", IS_PUBLIC);
 // eslint-disable-next-line no-undef
 export const DiceRollParser = IS_PUBLIC ? null : new DiceParser();
 // eslint-disable-next-line no-undef
