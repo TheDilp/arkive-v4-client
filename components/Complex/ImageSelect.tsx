787e6143be1f9d6646892b8fcaeca4e0550fcd11
Mihailo <mihailo.dilparic@gmail.com>
2024-12-24 13:26:16 +0100

    fix: image select btn pos

--- components/Complex/ImageSelect.tsx
+++ components/Complex/ImageSelect.tsx
@@ -24,8 +24,8 @@ export function ImageSelect({
   const permissions = useHasPermissions(["read_assets", "create_assets"], undefined);
 
   return (
-    <div className="flex w-full flex-col">
-      <div className="grid grid-cols-6 gap-x-2">
+    <div className="h flex w-full flex-col">
+      <div className="grid grid-cols-6 items-center gap-x-2">
         <div className={isIconOnly ? "col-span-5" : "col-span-4"}>
           {value ? (
             <EntityPreview
@@ -68,7 +68,7 @@ export function ImageSelect({
           )}
         </div>
 
-        <div className={`self-end ${isIconOnly ? "col-span-1" : "col-span-2"}`}>
+        <div className={`mt-0.5 max-h-full [&>button]:max-h-9 ${isIconOnly ? "col-span-1" : "col-span-2"}`}>
           <Button
             icon={IconEnum.upload}
             isDisabled={isDisabled || (!permissions?.create_assets && !isIgnoringPermissions)}
